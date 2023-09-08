41d89703750cbaa45e0fd0c536fc3d52ba05c89f
Steven Mackey <stevester94@gmail.com>
2023-09-07 18:44:06 -0700

    Start on quantity class

--- script.js
+++ script.js
@@ -1,8 +1,168 @@
-// TODO:
-// 2023-09-09:
+// Log:
+// 2023-09-05:
 //   - Gave up at unit conversions. It's not so hard, just tedious. convertUnits is going in the right direction, but need to return the
 //   conversion factor so subsequent scaling can be done for nutrition
 //   - addIngredient is broken. Adding that list of ingredients breaks. Could be something to do with spaces
+// 2023-09-07:
+//   - Started implementing Quantity class. Add a findRatio( newQuantity ) method and we should be good
+
+class Quantity {
+    constructor() {
+        this.value = null;
+        this.unit  = null;
+    }
+
+    fromVals( value, unit ) {
+        this.value = value;
+        this.unit  = unit;
+    }
+
+    fromStr( str ) {
+        // Get the units of a str (Just look at the suffix)
+
+        // let validUnits = [
+        //     "g",
+        //     "ml",
+        //     "l",
+        //     "kg",
+        //     "lb",
+        //     "oz",
+        //     "tbsp",
+        //     "tsp",
+        // ]
+
+        // Match a potential decimal scalar, potential unlimited spaces, then text suffix
+        let reg = /(\d+\.?\d*)\s*(.*)/
+        let matches = str.match( reg )
+        if( matches === null ) {
+            this.value = null;
+            this.unit  = null;
+            throw new Error("Couldnt parse <scalar value><alphanumeric unit>");
+        }
+        else {
+            this.value = parseFloat(matches[1]);
+            this.unit  = matches[2];
+        }
+    }
+
+    unitIsWeight( unit ) {
+        let weightUnits = [ "g", "kg", "lb", "oz" ]
+
+        return weightUnits.includes( unit );
+    }
+
+    unitIsVolume( unit ) {
+        let volumeUnits = [ "ml", "l", "tbsp", "tsp" ]
+
+        return volumeUnits.includes( unit );
+    }
+
+    convertQuantity( newUnit ) {
+        // Start by converting the original into grams or liters
+        let thisUnitType = null;
+        if( this.unitIsVolume(this.unit))
+            thisUnitType = "volume";
+        else if( this.unitIsWeight(this.unit))
+            thisUnitType = "weight";
+        else
+            thisUnitType = "custom";
+
+        if( thisUnitType == "volume" && ! this.unitIsVolume(newUnit) )
+            throw new Error("Original unit is a volume, but new unit is not");
+        
+        if( thisUnitType == "weight" && ! this.unitIsWeight(newUnit) )
+            throw new Error("Original unit is a weight, but new unit is not");
+
+        if( thisUnitType == "custom" && newUnit != this.unit )
+            throw new Error("Original unit is a custom unit, but new unit does not match");
+
+
+        if( thisUnitType == "weight" ) {
+            let scaleFactor = 0;
+
+            switch( this.unit ) {
+                case "g": scaleFactor = 1.0; break;
+                case "kg": scaleFactor = 1000.0; break;
+                case "lb": scaleFactor = 453.592; break;
+                case "oz": scaleFactor = 28.3495; break;
+                default: throw new Error(`Unsupported unit ${this.unit}`);
+            }
+            
+            switch( newUnit ) {
+                case "g": scaleFactor /= 1.0; break;
+                case "kg": scaleFactor /= 1000.0; break;
+                case "lb": scaleFactor /= 453.592; break;
+                case "oz": scaleFactor /= 28.3495; break;
+            }
+
+            let newQ = new Quantity();
+            newQ.fromVals( this.value*scaleFactor, newUnit );
+            return [ newQ, scaleFactor ];
+        }
+
+        else if( thisUnitType == "volume" ) {
+            let scaleFactor = 0;
+
+            switch( this.unit ) {
+                case "ml": scaleFactor   = 0.001; break;
+                case "l": scaleFactor    = 1.0; break;
+                case "tbsp": scaleFactor = 0.0147868; break;
+                case "tsp": scaleFactor  = 0.00492892; break;
+            }
+
+            switch( newUnit ) {
+                case "ml": scaleFactor   /= 0.001; break;
+                case "l": scaleFactor    /= 1.0; break;
+                case "tbsp": scaleFactor /= 0.0147868; break;
+                case "tsp": scaleFactor  /= 0.00492892; break;
+            }
+
+            let newQ = new Quantity();
+            newQ.fromVals( this.value*scaleFactor, newUnit );
+            return [ newQ, scaleFactor ];
+        }
+        else if( thisUnitType == "custom") {
+            let newQ = new Quantity();
+            newQ.fromVals( this.value, newUnit );
+            return [ newQ, 1.0 ];
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Your initialization code here
+
+    // console.log( I_O.getIngredient( "chicken1337" ) )
+
+    // console.log( I_O.getUnit( "1337ml" ) )
+    // console.log( I_O.getUnit( "1337kek" ) )
+
+    // console.log( I_O.convertUnits( "g", "oz", 28.3495 ) )
+
+    // console.log( I_O.getScalarFromQuantity( "1337.24asdf" ) )
+    // console.log( I_O.getScalarFromQuantity( "asdf" ) )
+
+    // // Get references to HTML elements
+    // const searchInput = document.getElementById("searchInput");
+    // const dropdown = document.getElementById("dropdown");
+
+    // // Event listener to handle item selection
+    // dropdown.addEventListener("click", searchClickHandler );
+    // searchInput.addEventListener("input", searchInputHandler );
+
+    // addIngredient( "whatever", "slop", "1337ml", 420, 59 )
+
+    // Other initialization tasks
+
+
+    var q1 = new Quantity();
+    q1.fromStr( "2.3 kg" )
+    console.log(q1)
+
+    // q2.convertQuantity( "tbsp" )
+    newQ = q1.convertQuantity( "lb" )
+    console.log( newQ[0], newQ[1] )
+});
 
 class Ingredient_Omnissiah {
     constructor() {
@@ -39,7 +199,7 @@ class Ingredient_Omnissiah {
     }
 
     getQuantityIngredient( id, quantity ) {
-        ingredient = this.getIngredient( id );
+        let ingredient = this.getIngredient( id );
         let originalUnit = this.getUnit( ingredient.quantity );
         let newUnit = this.getUnit( quantity );
 
@@ -54,141 +214,21 @@ class Ingredient_Omnissiah {
 
 
     }
-
-    // Get the units of a str (Just look at the suffix)
-    getUnit( s ) {
-        let validUnits = [
-            "g",
-            "ml",
-            "l",
-            "kg",
-            "lb",
-            "oz",
-            "tbsp",
-            "tsp",
-        ]
-
-        for( let i=0; i < validUnits.length; i++ ) {
-            let regex = new RegExp(`${validUnits[i]}$`)
-            if( regex.test( s )) {
-                return validUnits[i];
-            }
-        }
-        
-        return null;
-    }
-
-    unitIsWeight( unit ) {
-        let weightUnits = [ "g", "kg", "lb", "oz" ]
-
-        return weightUnits.includes( unit );
-    }
-
-    unitIsVolume( unit ) {
-        let volumeUnits = [ "ml", "l", "tbsp", "tsp" ]
-
-        return volumeUnits.includes( unit );
-    }
-
-    getScalarFromQuantity( quantity ) {
-        let reg = /([0-9]*\.[0-9]).*/
-        let matches = quantity.match( reg )
-        if( matches === null ) {
-            return null;
-        }
-        else {
-            return matches[1];
-        }
-    }
-
-    convertUnits( originalUnit, newUnit, originalQuantity ) {
-        // Start by converting the original into grams or liters
-        if( this.unitIsWeight( originalUnit )) {
-            if( ! this.unitIsWeight( newUnit )) {
-                return null;
-            }
-
-            let originalInGrams = originalQuantity;
-
-            switch( originalUnit ) {
-                case "g": originalInGrams *= 1.0; break;
-                case "kg": originalInGrams *= 1000.0; break;
-                case "lb": originalInGrams *= 453.592; break;
-                case "oz": originalInGrams *= 28.3495; break;
-            }
-            
-            switch( newUnit ) {
-                case "g": return originalInGrams / 1.0;
-                case "kg": return originalInGrams / 1000.0;
-                case "lb": return originalInGrams / 453.592;
-                case "oz": return originalInGrams / 28.3495;
-            }
-        }
-        else if( this.unitIsVolume( originalUnit )) {
-            if( ! this.unitIsVolume( newUnit )) {
-                return null;
-            }
-
-            let originalInLiters = originalQuantity;
-
-            switch( originalUnit ) {
-                case "ml": originalInLiters *= 0.001; break;
-                case "l": originalInLiters *= 1.0; break;
-                case "tbsp": originalInLiters *= 0.0147868; break;
-                case "tsp": originalInLiters *= 0.00492892; break;
-            }
-
-            switch( newUnit ) {
-                case "ml": return originalInLiters / 0.001;
-                case "l": return originalInLiters / 1.0;
-                case "tbsp": return originalInLiters / 0.0147868;
-                case "tsp": return originalInLiters / 0.00492892;
-            }
-        }
-        else {
-            return null;
-        }
-    }
-
 }
 
 
 
 
 var I_O = new Ingredient_Omnissiah();
-I_O.addIngredient( "chicken", "Chicken", "110g", 30, 220 );
-I_O.addIngredient( "Chobani FF Vanilla", "Chobani FF Vanilla",          "10g", 110, 12 )
-I_O.addIngredient( "1 Scoop Shake w/ Collagen", "1 Scoop Shake w/ Collagen",   "10g", 165, 35 )
-I_O.addIngredient( "Bacon, 2 medium slices", "Bacon, 2 medium slices",      "10g", 86, 6 )
-I_O.addIngredient( "Egg", "Egg",                         "10g", 70, 6 )
-I_O.addIngredient( "1 cup Grated Cheddar", "1 cup Grated Cheddar",        "10g", 480, 24 )
-I_O.addIngredient( "1 cup heavy cream", "1 cup heavy cream",           "10g", 821, 4.9 )
+// I_O.addIngredient( "chicken", "Chicken", "110g", 30, 220 );
+// I_O.addIngredient( "Chobani FF Vanilla", "Chobani FF Vanilla",          "10g", 110, 12 )
+// I_O.addIngredient( "1 Scoop Shake w/ Collagen", "1 Scoop Shake w/ Collagen",   "10g", 165, 35 )
+// I_O.addIngredient( "Bacon, 2 medium slices", "Bacon, 2 medium slices",      "10g", 86, 6 )
+// I_O.addIngredient( "Egg", "Egg",                         "10g", 70, 6 )
+// I_O.addIngredient( "1 cup Grated Cheddar", "1 cup Grated Cheddar",        "10g", 480, 24 )
+// I_O.addIngredient( "1 cup heavy cream", "1 cup heavy cream",           "10g", 821, 4.9 )
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Your initialization code here
 
-    console.log( I_O.getIngredient( "chicken1337" ) )
-
-    console.log( I_O.getUnit( "1337ml" ) )
-    console.log( I_O.getUnit( "1337kek" ) )
-
-    console.log( I_O.convertUnits( "g", "oz", 28.3495 ) )
-
-    console.log( I_O.getScalarFromQuantity( "1337.24asdf" ) )
-    console.log( I_O.getScalarFromQuantity( "asdf" ) )
-
-    // Get references to HTML elements
-    const searchInput = document.getElementById("searchInput");
-    const dropdown = document.getElementById("dropdown");
-
-    // Event listener to handle item selection
-    dropdown.addEventListener("click", searchClickHandler );
-    searchInput.addEventListener("input", searchInputHandler );
-
-    addIngredient( "whatever", "slop", "1337ml", 420, 59 )
-
-    // Other initialization tasks
-});
 
 
 // Function to add a new task to the list
